1a5f966f077cd60dec377dc44198bbf466686b19
georgewbrown <396gbrown@gmail.com>
2019-08-21 10:08:18 -0400

    inital commit

---
+++ dropdown.js
@@ -0,0 +1,406 @@
+const locations = [{
+    "location_id": "1",
+    "location": "Online",
+    "city": "",
+    "state": "",
+    "display_label": "Online"
+  },
+  {
+    "location_id": "2",
+    "location": "Bowling Green",
+    "city": "Bowling Green",
+    "state": "Kentucky",
+    "display_label": "Bowling Green, KY"
+  },
+  {
+    "location_id": "3",
+    "location": "Columbus",
+    "city": "Columbus",
+    "state": "Ohio",
+    "display_label": "Columbus, OH"
+  },
+  {
+    "location_id": "4",
+    "location": "Clarksville",
+    "city": "Clarksville",
+    "state": "Tennessee",
+    "display_label": "Clarksville, TN"
+  },
+  {
+    "location_id": "5",
+    "location": "Murfreesboro",
+    "city": "Murfreesboro",
+    "state": "Tennessee",
+    "display_label": "Murfreesboro, TN"
+  },
+  {
+    "location_id": "6",
+    "location": "Nashville",
+    "city": "Nashville",
+    "state": "Tennessee",
+    "display_label": "Nashville, TN"
+  }
+];
+
+const programs = [{
+    "id": "",
+    "program_id": "116",
+    "location_id": "1",
+    "program": "Not Sure"
+  },
+  {
+    "id": "",
+    "program_id": "116",
+    "location_id": "2",
+    "program": "Not Sure"
+  },
+  {
+    "id": "",
+    "program_id": "116",
+    "location_id": "3",
+    "program": "Not Sure"
+  },
+  {
+    "id": "",
+    "program_id": "116",
+    "location_id": "4",
+    "program": "Not Sure"
+  },
+  {
+    "id": "",
+    "program_id": "116",
+    "location_id": "5",
+    "program": "Not Sure"
+  },
+  {
+    "id": "",
+    "program_id": "116",
+    "location_id": "6",
+    "program": "Not Sure"
+  },
+  {
+    "id": "",
+    "program_id": "101",
+    "location_id": "2",
+    "program": "Accounting"
+  },
+  {
+    "id": "",
+    "program_id": "101",
+    "location_id": "5",
+    "program": "Accounting"
+  },
+  {
+    "id": "",
+    "program_id": "102",
+    "location_id": "1",
+    "program": "Billing & Coding Specialist"
+  },
+  {
+    "id": "",
+    "program_id": "102",
+    "location_id": "2",
+    "program": "Billing & Coding Specialist"
+  },
+  {
+    "id": "",
+    "program_id": "102",
+    "location_id": "3",
+    "program": "Billing & Coding Specialist"
+  },
+  {
+    "id": "",
+    "program_id": "102",
+    "location_id": "4",
+    "program": "Billing & Coding Specialist"
+  },
+  {
+    "id": "",
+    "program_id": "102",
+    "location_id": "5",
+    "program": "Billing & Coding Specialist"
+  },
+  {
+    "id": "",
+    "program_id": "102",
+    "location_id": "6",
+    "program": "Billing & Coding Specialist"
+  },
+  {
+    "id": "",
+    "program_id": "103",
+    "location_id": "1",
+    "program": "Business Management"
+  },
+  {
+    "id": "",
+    "program_id": "103",
+    "location_id": "2",
+    "program": "Business Management"
+  },
+  {
+    "id": "",
+    "program_id": "103",
+    "location_id": "3",
+    "program": "Business Management"
+  },
+  {
+    "id": "",
+    "program_id": "103",
+    "location_id": "4",
+    "program": "Business Management"
+  },
+  {
+    "id": "",
+    "program_id": "103",
+    "location_id": "5",
+    "program": "Business Management"
+  },
+  {
+    "id": "",
+    "program_id": "103",
+    "location_id": "3",
+    "program": "Business Management"
+  },
+  {
+    "id": "",
+    "program_id": "104",
+    "location_id": "2",
+    "program": "Cardiographic Technician"
+  },
+  {
+    "id": "",
+    "program_id": "105",
+    "location_id": "1",
+    "program": "Criminal Justice Administration"
+  },
+  {
+    "id": "",
+    "program_id": "105",
+    "location_id": "2",
+    "program": "Criminal Justice Administration"
+  },
+  {
+    "id": "",
+    "program_id": "105",
+    "location_id": "4",
+    "program": "Criminal Justice Administration"
+  },
+  {
+    "id": "",
+    "program_id": "106",
+    "location_id": "1",
+    "program": "Criminal Justice"
+  },
+  {
+    "id": "",
+    "program_id": "106",
+    "location_id": "2",
+    "program": "Criminal Justice"
+  },
+  {
+    "id": "",
+    "program_id": "106",
+    "location_id": "4",
+    "program": "Criminal Justice"
+  },
+  {
+    "id": "",
+    "program_id": "106",
+    "location_id": "5",
+    "program": "Criminal Justice"
+  },
+  {
+    "id": "",
+    "program_id": "107",
+    "location_id": "4",
+    "program": "Dental Assisting"
+  },
+  {
+    "id": "",
+    "program_id": "108",
+    "location_id": "1",
+    "program": "Healthcare Administration"
+  },
+  {
+    "id": "",
+    "program_id": "108",
+    "location_id": "4",
+    "program": "Healthcare Administration"
+  },
+  {
+    "id": "",
+    "program_id": "108",
+    "location_id": "6",
+    "program": "Healthcare Administration"
+  },
+  {
+    "id": "",
+    "program_id": "109",
+    "location_id": "1",
+    "program": "Human Resource Management"
+  },
+  {
+    "id": "",
+    "program_id": "109",
+    "location_id": "4",
+    "program": "Human Resource Management"
+  },
+  {
+    "id": "",
+    "program_id": "110",
+    "location_id": "3",
+    "program": "Medical Massage Therapy"
+  },
+  {
+    "id": "",
+    "program_id": "111",
+    "location_id": "2",
+    "program": "Medical Assisting"
+  },
+  {
+    "id": "",
+    "program_id": "111",
+    "location_id": "3",
+    "program": "Medical Assisting"
+  },
+  {
+    "id": "",
+    "program_id": "111",
+    "location_id": "4",
+    "program": "Medical Assisting"
+  },
+  {
+    "id": "",
+    "program_id": "111",
+    "location_id": "5",
+    "program": "Medical Assisting"
+  },
+  {
+    "id": "",
+    "program_id": "111",
+    "location_id": "6",
+    "program": "Medical Assisting"
+  },
+  {
+    "id": "",
+    "program_id": "112",
+    "location_id": "2",
+    "program": "Network Support Administration"
+  },
+  {
+    "id": "",
+    "program_id": "112",
+    "location_id": "4",
+    "program": "Network Support Administration"
+  },
+  {
+    "id": "",
+    "program_id": "112",
+    "location_id": "5",
+    "program": "Network Support Administration"
+  },
+  {
+    "id": "",
+    "program_id": "112",
+    "location_id": "6",
+    "program": "Network Support Administration"
+  },
+  {
+    "id": "",
+    "program_id": "113",
+    "location_id": "1",
+    "program": "Paralegal Studies"
+  },
+  {
+    "id": "",
+    "program_id": "113",
+    "location_id": "4",
+    "program": "Paralegal Studies"
+  },
+  {
+    "id": "",
+    "program_id": "114",
+    "location_id": "1",
+    "program": "Pharmacy Technology"
+  },
+  {
+    "id": "",
+    "program_id": "114",
+    "location_id": "2",
+    "program": "Pharmacy Technology"
+  },
+  {
+    "id": "",
+    "program_id": "114",
+    "location_id": "3",
+    "program": "Pharmacy Technology"
+  },
+  {
+    "id": "",
+    "program_id": "114",
+    "location_id": "4",
+    "program": "Pharmacy Technology"
+  },
+  {
+    "id": "",
+    "program_id": "114",
+    "location_id": "5",
+    "program": "Pharmacy Technology"
+  }, {
+    "id": "",
+    "program_id": "114",
+    "location_id": "6",
+    "program": "Pharmacy Technology"
+  },
+  {
+    "id": "",
+    "program_id": "115",
+    "location_id": "4",
+    "program": "Physical Therapist Assistant"
+  }
+
+];
+
+
+const locationSelection = document.getElementsByTagName('select')[0];
+const programsSelection = document.getElementsByTagName('select')[1];
+
+locationSelection.addEventListener('change', (() => {
+  programsSelection.innerHTML = `<option></option>`
+  const userSelection = locationSelection.value;
+
+  function filterPrograms(locationID) {
+    const locationPrograms = programs
+      .filter(program => program.location_id === locationID)
+      .map(program => program.program);
+    for (j = 1; j <= locationPrograms.length - 1; j++) {
+      const programOption = locationPrograms[j];
+      let option = document.createElement('option');
+      option.text = programOption;
+      programsSelection.appendChild(option);
+    };
+  };
+
+  switch(userSelection) {
+    case 'Online':
+      filterPrograms('1');
+      break;
+    case 'Bowling Green':
+      filterPrograms('2');
+      break;
+    case 'Columbus':
+      filterPrograms('3');
+      break;
+    case 'Clarksville':
+      filterPrograms('4');
+      break;
+    case 'Murfreesboro':
+      filterPrograms('5');
+      break;
+    case 'Nashville':
+      filterPrograms('6');
+      break;
+  };
+
+}));
