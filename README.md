3926f9fe092bfddfc5699681247f46f473b529a1
georgewbrown <396gbrown@gmail.com>
2019-09-03 11:14:34 -0400

    MODIFIED TO BE BOILERPLATE AND UPDATED README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Boilerplate Dropdown
+
+A template for a dropdown
+
+## Get Started
+
+Either Clone this repo or Copy and paste the contents of this repository.
+
+## Usage
+
+This is a dyanmic dropdown for a College location's and their program's. Where the location selected dynamically loads the programs of the selected location.
+Please either import or copy and paste your needed json into the corresponding `locations` and `programs` constants. This will make for the least amount of modification.
+```javascript
+const locations = []; // add your location json
+const programs = []; // add your program json
+```
+You will also need to add the `location` names to the switch statment. Please make sure each case matches the dropdowns text, this will allow the code to function as intended.
+```javascript
+  switch(userSelection) { //checks which case matched the user's selection and passes the corresponding locaiton id to the filter programs fucntion
+    case 'LOCATION_1': //ENTER YOUR LOCATIONS NAME
+      filterPrograms('1');
+      ...
+```
+### If Using a CMS
+If you are using a content management system, you may need to enable `strict mode` and or be wrap the script in a explicit script tag
+```javascript
+<script type="text/javascript">
+YOUR CODE HERE
+</script>
--- dropdown.js
+++ dropdown.js
@@ -1,410 +1,44 @@
-const locations = [{
-    "location_id": "1",
-    "location": "Online",
-    "city": "",
-    "state": "",
-    "display_label": "Online"
-  },
-  {
-    "location_id": "2",
-    "location": "Bowling Green",
-    "city": "Bowling Green",
-    "state": "Kentucky",
-    "display_label": "Bowling Green, KY"
-  },
-  {
-    "location_id": "3",
-    "location": "Columbus",
-    "city": "Columbus",
-    "state": "Ohio",
-    "display_label": "Columbus, OH"
-  },
-  {
-    "location_id": "4",
-    "location": "Clarksville",
-    "city": "Clarksville",
-    "state": "Tennessee",
-    "display_label": "Clarksville, TN"
-  },
-  {
-    "location_id": "5",
-    "location": "Murfreesboro",
-    "city": "Murfreesboro",
-    "state": "Tennessee",
-    "display_label": "Murfreesboro, TN"
-  },
-  {
-    "location_id": "6",
-    "location": "Nashville",
-    "city": "Nashville",
-    "state": "Tennessee",
-    "display_label": "Nashville, TN"
-  }
-];
+const locations = []; // add your location json
+const programs = []; // add your program json
 
-const programs = [{
-    "id": "",
-    "program_id": "116",
-    "location_id": "1",
-    "program": "Not Sure"
-  },
-  {
-    "id": "",
-    "program_id": "116",
-    "location_id": "2",
-    "program": "Not Sure"
-  },
-  {
-    "id": "",
-    "program_id": "116",
-    "location_id": "3",
-    "program": "Not Sure"
-  },
-  {
-    "id": "",
-    "program_id": "116",
-    "location_id": "4",
-    "program": "Not Sure"
-  },
-  {
-    "id": "",
-    "program_id": "116",
-    "location_id": "5",
-    "program": "Not Sure"
-  },
-  {
-    "id": "",
-    "program_id": "116",
-    "location_id": "6",
-    "program": "Not Sure"
-  },
-  {
-    "id": "",
-    "program_id": "101",
-    "location_id": "2",
-    "program": "Accounting"
-  },
-  {
-    "id": "",
-    "program_id": "101",
-    "location_id": "5",
-    "program": "Accounting"
-  },
-  {
-    "id": "",
-    "program_id": "102",
-    "location_id": "1",
-    "program": "Billing & Coding Specialist"
-  },
-  {
-    "id": "",
-    "program_id": "102",
-    "location_id": "2",
-    "program": "Billing & Coding Specialist"
-  },
-  {
-    "id": "",
-    "program_id": "102",
-    "location_id": "3",
-    "program": "Billing & Coding Specialist"
-  },
-  {
-    "id": "",
-    "program_id": "102",
-    "location_id": "4",
-    "program": "Billing & Coding Specialist"
-  },
-  {
-    "id": "",
-    "program_id": "102",
-    "location_id": "5",
-    "program": "Billing & Coding Specialist"
-  },
-  {
-    "id": "",
-    "program_id": "102",
-    "location_id": "6",
-    "program": "Billing & Coding Specialist"
-  },
-  {
-    "id": "",
-    "program_id": "103",
-    "location_id": "1",
-    "program": "Business Management"
-  },
-  {
-    "id": "",
-    "program_id": "103",
-    "location_id": "2",
-    "program": "Business Management"
-  },
-  {
-    "id": "",
-    "program_id": "103",
-    "location_id": "3",
-    "program": "Business Management"
-  },
-  {
-    "id": "",
-    "program_id": "103",
-    "location_id": "4",
-    "program": "Business Management"
-  },
-  {
-    "id": "",
-    "program_id": "103",
-    "location_id": "5",
-    "program": "Business Management"
-  },
-  {
-    "id": "",
-    "program_id": "103",
-    "location_id": "3",
-    "program": "Business Management"
-  },
-  {
-    "id": "",
-    "program_id": "104",
-    "location_id": "2",
-    "program": "Cardiographic Technician"
-  },
-  {
-    "id": "",
-    "program_id": "105",
-    "location_id": "1",
-    "program": "Criminal Justice Administration"
-  },
-  {
-    "id": "",
-    "program_id": "105",
-    "location_id": "2",
-    "program": "Criminal Justice Administration"
-  },
-  {
-    "id": "",
-    "program_id": "105",
-    "location_id": "4",
-    "program": "Criminal Justice Administration"
-  },
-  {
-    "id": "",
-    "program_id": "106",
-    "location_id": "1",
-    "program": "Criminal Justice"
-  },
-  {
-    "id": "",
-    "program_id": "106",
-    "location_id": "2",
-    "program": "Criminal Justice"
-  },
-  {
-    "id": "",
-    "program_id": "106",
-    "location_id": "4",
-    "program": "Criminal Justice"
-  },
-  {
-    "id": "",
-    "program_id": "106",
-    "location_id": "5",
-    "program": "Criminal Justice"
-  },
-  {
-    "id": "",
-    "program_id": "107",
-    "location_id": "4",
-    "program": "Dental Assisting"
-  },
-  {
-    "id": "",
-    "program_id": "108",
-    "location_id": "1",
-    "program": "Healthcare Administration"
-  },
-  {
-    "id": "",
-    "program_id": "108",
-    "location_id": "4",
-    "program": "Healthcare Administration"
-  },
-  {
-    "id": "",
-    "program_id": "108",
-    "location_id": "6",
-    "program": "Healthcare Administration"
-  },
-  {
-    "id": "",
-    "program_id": "109",
-    "location_id": "1",
-    "program": "Human Resource Management"
-  },
-  {
-    "id": "",
-    "program_id": "109",
-    "location_id": "4",
-    "program": "Human Resource Management"
-  },
-  {
-    "id": "",
-    "program_id": "110",
-    "location_id": "3",
-    "program": "Medical Massage Therapy"
-  },
-  {
-    "id": "",
-    "program_id": "111",
-    "location_id": "2",
-    "program": "Medical Assisting"
-  },
-  {
-    "id": "",
-    "program_id": "111",
-    "location_id": "3",
-    "program": "Medical Assisting"
-  },
-  {
-    "id": "",
-    "program_id": "111",
-    "location_id": "4",
-    "program": "Medical Assisting"
-  },
-  {
-    "id": "",
-    "program_id": "111",
-    "location_id": "5",
-    "program": "Medical Assisting"
-  },
-  {
-    "id": "",
-    "program_id": "111",
-    "location_id": "6",
-    "program": "Medical Assisting"
-  },
-  {
-    "id": "",
-    "program_id": "112",
-    "location_id": "2",
-    "program": "Network Support Administration"
-  },
-  {
-    "id": "",
-    "program_id": "112",
-    "location_id": "4",
-    "program": "Network Support Administration"
-  },
-  {
-    "id": "",
-    "program_id": "112",
-    "location_id": "5",
-    "program": "Network Support Administration"
-  },
-  {
-    "id": "",
-    "program_id": "112",
-    "location_id": "6",
-    "program": "Network Support Administration"
-  },
-  {
-    "id": "",
-    "program_id": "113",
-    "location_id": "1",
-    "program": "Paralegal Studies"
-  },
-  {
-    "id": "",
-    "program_id": "113",
-    "location_id": "4",
-    "program": "Paralegal Studies"
-  },
-  {
-    "id": "",
-    "program_id": "114",
-    "location_id": "1",
-    "program": "Pharmacy Technology"
-  },
-  {
-    "id": "",
-    "program_id": "114",
-    "location_id": "2",
-    "program": "Pharmacy Technology"
-  },
-  {
-    "id": "",
-    "program_id": "114",
-    "location_id": "3",
-    "program": "Pharmacy Technology"
-  },
-  {
-    "id": "",
-    "program_id": "114",
-    "location_id": "4",
-    "program": "Pharmacy Technology"
-  },
-  {
-    "id": "",
-    "program_id": "114",
-    "location_id": "5",
-    "program": "Pharmacy Technology"
-  }, {
-    "id": "",
-    "program_id": "114",
-    "location_id": "6",
-    "program": "Pharmacy Technology"
-  },
-  {
-    "id": "",
-    "program_id": "115",
-    "location_id": "4",
-    "program": "Physical Therapist Assistant"
-  }
+const locationSelection = document.getElementsByTagName('select')[0]; // grabs the first dropdown on the page and sets it for the list of locations
+const programsSelection = document.getElementsByTagName('select')[1]; // grabs the second dropdown on the page and sets it for the list of programs
 
-];
+locationSelection.addEventListener('change', (() => { // listens for any change on the location dropdown
+  programsSelection.innerHTML = `<option></option>`; // removes the previous location's programs from the program dropdown everytime a new location is selected
+  const userSelection = locationSelection.value; // gets value of the location selected
 
-document.addEventListener('DOMContentLoaded', (() => {
-
-const locationSelection = document.getElementsByTagName('select')[0];
-const programsSelection = document.getElementsByTagName('select')[1];
-
-locationSelection.addEventListener('change', (() => {
-  programsSelection.innerHTML = `<option></option>`
-  const userSelection = locationSelection.value;
-  console.log(userSelection);
-
-  function filterPrograms(locationID) {
-    console.log(locationID);
-    const locationPrograms = programs
-      .filter(program => program.location_id === locationID)
-      .map(program => program.program);
-    for (j = 1; j <= locationPrograms.length - 1; j++) {
+  function filterPrograms(locationID) { // takes in a location id for each case set by the switch statement below
+    const locationPrograms = programs 
+      .filter(program => program.location_id === locationID) // gets only the programs with the same locaiton id as the school
+      .map(program => program.program); // creates a new array of the filterd programs names
+    for (j = 1; j <= locationPrograms.length - 1; j++) { // loops through each of the program names and appends them to the programs dropdown
       const programOption = locationPrograms[j];
-      let option = document.createElement('option');
-      option.text = programOption;
-      programsSelection.appendChild(option);
+      let option = document.createElement('option'); // creates a new option element
+      option.text = programOption; // sets that option element's text to be the current program's name
+      programsSelection.appendChild(option); // appends the current option to the programs dropdown
     };
   };
 
-  switch(userSelection) {
-    case 'Online':
+  switch(userSelection) { //checks which case matched the user's selection and passes the corresponding locaiton id to the filter programs fucntion
+    case 'LOCATION_1': //ENTER YOUR LOCATIONS NAME
       filterPrograms('1');
       break;
-    case 'Bowling Green, KY':
+    case 'LOCATION_2'://ENTER YOUR LOCATIONS NAME
       filterPrograms('2');
       break;
-    case 'Columbus, OH':
+    case 'LOCATION_3'://ENTER YOUR LOCATIONS NAME
       filterPrograms('3');
       break;
-    case 'Clarksville, TN':
+    case 'LOCATION_4'://ENTER YOUR LOCATIONS NAME
       filterPrograms('4');
       break;
-    case 'Murfreesboro, TN':
+    case 'LOCATION_5'://ENTER YOUR LOCATIONS NAME
       filterPrograms('5');
       break;
-    case 'Nashville, TN':
+    case 'LOCATION_6'://ENTER YOUR LOCATIONS NAME
       filterPrograms('6');
       break;
   };
 
-}));
 }));
